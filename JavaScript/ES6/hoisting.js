a50af2fb7ba703abb68d58a9767e9e0a8499361e
Sandhiya <sanjukrish2005@gmail.com>
2024-12-29 10:21:47 +0530

    adding index.html

---
+++ JavaScript/ES6/hoisting.js
@@ -0,0 +1,14 @@
+// eg 1
+console.log(a);
+var a=11;
+console.log(a);
+
+// eg 2
+console.log(a);
+const a=3.14
+console.log(a);
+
+// eg 3
+console.log(a);
+let a=11;
+console.log(a);
---
+++ JavaScript/ES6/ternaryoperators.js
@@ -0,0 +1,4 @@
+//Ternary operator
+a=10
+out=(a%2==0)?"Even":"odd"
+console.log(a,"is",out);
