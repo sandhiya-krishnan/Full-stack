0800b363907c316c742980445879ad60f9a5dd8e
Sandhiya <sanjukrish2005@gmail.com>
2024-12-29 10:16:02 +0530

    adding index.html

--- JavaScript/loopingStatements.js
+++ JavaScript/loopingStatements.js
@@ -1,5 +1,26 @@
 array = [10,20,30,[40.5,50.6],"a","KEC",true]
 console.log(array);
+//Example1
+array.forEach((index,element)=>{
+    console.log("index is",index,"element is",element)
+})
+
+//Example2
+obj = {
+    "firstName": "Sandhiya",
+    "lastName": "K",
+    "age": 19,
+}
+console.log(obj.age);
+for (key in obj)
+{
+    value=obj[key]
+    console.log(key,value)
+}
+Object.keys(obj).forEach(key => {
+    const value = obj[key];
+    console.log(key, value);
+})
 
 //1.for...in loop
 for (var a in array) {
